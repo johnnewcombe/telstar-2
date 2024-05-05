bb518635be2e5b9547fc8689780f854d816d9075
John Newcombe (GlassTTY) <jnewcombeuk@gmail.com>
2024-05-05 23:20:25 +0100

    Fixed Icon

--- telstar-client/gen.sh
+++ telstar-client/gen.sh
@@ -8,7 +8,7 @@ echo $DIR
 cd $DIR
 rm $FILE
 
-$BIN/fyne bundle -package constants -name AppIcon ./Icons/icon.png > $FILE
+$BIN/fyne bundle -package constants -name AppIcon ./icon.png > $FILE
 $BIN/fyne bundle -package constants -name SerialIcon -append ./Icons/phone-70-512.png >> $FILE
 $BIN/fyne bundle -package constants -name CloudIcon -append ./Icons/cloud-3-512.png >> $FILE
 $BIN/fyne bundle -package constants -name CloseIcon -append ./Icons/x-mark-4-512.png >> $FILE
